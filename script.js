455639c06b9e7fce3fae13ec0e04b5febda05631
Suanderson <123583417+Suanderson@users.noreply.github.com>
2026-03-09 22:33:00 -0300

    Update URL endpoint in fetch request

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function consultarOS(){
 let os = document.getElementById("os").value
 let telefone = document.getElementById("telefone").value
 
-let url = "https://script.google.com/macros/s/AKfycbyGIGOxXHGtxXj5b4WHxbIU7IluC-Y7JWuZR_o1Dz0/dev?acao=consultarOS&os="+os+"&telefone="+telefone
+let url = "https://script.google.com/macros/s/AKfycbyGIGOxXHGtxXj5b4WHxbIU7IluC-Y7JWuZR_o1Dz0/exec?acao=consultarOS&os="+os+"&telefone="+telefone
 
 fetch(url)
 .then(res => res.json())
@@ -11,6 +11,13 @@ fetch(url)
 
 document.getElementById("resultado").innerHTML =
 "<pre>"+JSON.stringify(dados,null,2)+"</pre>"
+
+})
+.catch(err => {
+
+document.getElementById("resultado").innerHTML = "Erro na consulta"
+
+console.log(err)
 
 })
 
