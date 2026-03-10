6f0aa0c05974963fb1e8d451c3a382b7e289cb74
Suanderson <123583417+Suanderson@users.noreply.github.com>
2026-03-09 21:43:36 -0300

    Fetch order status from Google Apps Script API

--- script.js
+++ script.js
@@ -3,7 +3,37 @@ function consultarOS(){
 let os = document.getElementById("os").value
 let telefone = document.getElementById("telefone").value
 
+let url = "https://script.google.com/macros/s/AKfycbyGIGOxXHGtxXj5b4WHxbIU7IluC-Y7JWuZR_o1Dz0/dev?os="+os+"&telefone="+telefone
+
+fetch(url)
+.then(res => res.json())
+.then(dados => {
+
+if(dados.status == "erro"){
+
 document.getElementById("resultado").innerHTML =
-"Consultando OS " + os
+"OS não encontrada"
+
+return
+
+}
+
+document.getElementById("resultado").innerHTML =
+
+`
+<h2>ORDEM DE SERVIÇO</h2>
+
+<h1>${dados.os}</h1>
+
+<p><b>Situação atual:</b><br>${dados.status}</p>
+
+<p><b>Serviço:</b><br>${dados.servico}</p>
+
+<p><b>Entrada:</b><br>${dados.entrada}</p>
+
+<p><b>Saída:</b><br>${dados.saida}</p>
+`
+
+})
 
 }
