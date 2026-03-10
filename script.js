d1a4850258e50955b3cf89cf2eab8f409905e3c1
Suanderson <123583417+Suanderson@users.noreply.github.com>
2026-03-09 21:37:18 -0300

    Add consultarOS function to handle OS consultation

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Sistema OS carregado");
+function consultarOS(){
+
+let os = document.getElementById("os").value
+let telefone = document.getElementById("telefone").value
+
+document.getElementById("resultado").innerHTML =
+"Consultando OS " + os
+
+}
