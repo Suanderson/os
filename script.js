6559dc7a3a9628816b007d7a9b85a4c92e76ae14
Suanderson <123583417+Suanderson@users.noreply.github.com>
2026-03-09 22:12:42 -0300

    Update URL and fetch response handling in script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function consultarOS(){
 let os = document.getElementById("os").value
 let telefone = document.getElementById("telefone").value
 
-let url = "https://script.google.com/macros/s/AKfycbyGIGOxXHGtxXj5b4WHxbIU7IluC-Y7JWuZR_o1Dz0/dev?os="+os+"&telefone="+telefone
+let url = "https://script.google.com/macros/s/AKfycbyGIGOxXHGtxXj5b4WHxbIU7IluC-Y7JWuZR_o1Dz0/dev?acao=consultarOS&os="+os+"&telefone="+telefone
 
 fetch(url)
-.then(response => response.json())
+.then(res => res.json())
 .then(dados => {
 
 document.getElementById("resultado").innerHTML =
-"<pre>"+JSON.stringify(dados, null, 2)+"</pre>"
+"<pre>"+JSON.stringify(dados,null,2)+"</pre>"
 
 })
 
