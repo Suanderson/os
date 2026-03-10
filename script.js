0cf058b9d93726b9a619cad2c1fdfe37c2fb3932
Suanderson <123583417+Suanderson@users.noreply.github.com>
2026-03-09 21:58:43 -0300

    Implement OS consultation with fetch request
    Add fetch request to consult OS and display results

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function consultarOS(){
 
-alert("Botão funcionando")
+let os = document.getElementById("os").value
+let telefone = document.getElementById("telefone").value
+
+let url = "https://script.google.com/macros/s/AKfycbyGIGOxXHGtxXj5b4WHxbIU7IluC-Y7JWuZR_o1Dz0/dev?os="+os+"&telefone="+telefone
+
+fetch(url)
+.then(response => response.json())
+.then(dados => {
+
+document.getElementById("resultado").innerHTML =
+"<pre>"+JSON.stringify(dados, null, 2)+"</pre>"
+
+})
 
 }
